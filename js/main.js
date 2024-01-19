6bdade091bea689dcfd887eada5049e7cd7be229
Houdayfa <91084419+Aschemlal@users.noreply.github.com>
2024-01-19 16:47:47 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let div = document.getElementById("mouse")
-let titre = document.getElementById("titre")
-
-document.addEventListener("mousemove", function(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    div.style.left = x + "px"
-    div.style.top = y + "px"
-})
